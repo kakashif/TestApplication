4ae4228f7764cb5fec42115fec09d57d777c3884
kkhanusat <kkhan@usatoday.com>
2013-07-31 19:29:54 -0400

    Updating script

--- powershell/ps.ps1
+++ powershell/ps.ps1
@@ -56,17 +56,6 @@ Function AddAttribute
     }
 }
 
-Function CreateSeoTitle()
-{
-    $seoTitle = ""
-    $seoTitleRegEx = New-Object "System.Collections.Generic.List[string[]]"
-    $seoTitleRegEx.Add(@("{\d+}$", ""))
-    if (ApplyXPathAndRegEx "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_summary__2" $seoTitleRegEx ([ref]$seoTitle))
-    {
-        $attributes.Add("seotitle", $seoTitle)
-    }
-}
-
 Function CreateSeoName()
 {
     $seoName = ""
@@ -85,17 +74,6 @@ Function CreateSeoName()
     if ($seoName -NE "")
     {
         $attributes.Add("seoname", $seoName.ToLower())
-    }
-}
-
-Function CreatePhotoHead()
-{
-    $photoHead = ""
-    $photoHeadRegEx = New-Object "System.Collections.Generic.List[string[]]"
-    $photoHeadRegEx.Add(@("{\d+}$", ""))
-    if (ApplyXPathAndRegEx "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_head__photo" $photoHeadRegEx ([ref]$photoHead))
-    {
-        $attributes.Add("hphoto-hed", $photoHead)
     }
 }
 
@@ -118,51 +96,113 @@ Function CreateShortHed()
     }
 }
 
-Function CreateChatter()
+Function CreateSimpleAttributes()
 {
+    AddAttribute "seotitle" "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_summary__2"
+    AddAttribute "hphoto-hed" "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_head__photo"
     AddAttribute "chatter" "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_summary__chatter"
-}
-
-Function CreateMetaDescription()
-{
     AddAttribute "metadescription" "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='meta_description']"
+    AddAttribute "storyhighlights" "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='story_highlights']"
+    AddAttribute "createdBy" "//object/children/object[@kind='Text']/attributes/attribute[@name='CreateOperator']"
+    AddAttribute "createTime" "//object/children/object[@kind='Text']/attributes/attribute[@name='CreateTime']"
+    AddAttribute "updatedBy" "//object/children/object[@kind='Text']/attributes/attribute[@name='RevOperator']"
+    AddAttribute "updateTime" "//object/children/object[@kind='Text']/attributes/attribute[@name='LastRev']"
+    AddAttribute "source" "//object/children/object[@kind='Text']/attributes/attribute[@name='IIM_Source']"
+    #AddAttribute "" ""
+    
 }
 
-Function CreateAttributes($xmlData)
+Function CreateAttributes()
 {
     # using DOT notation will probably confuse people therefore using XPath statements instead
     # here is equivalent of origin key using dot notation: ($xmlData.CCIObjects.object | Where kind -EQ "Budget").Attributes.Attribute | Where name -EQ "Id"
     
     # get origin key and cci id
-    $originkey = ""
-    if ((ApplyXPath "//object[@kind='Budget']/attributes/attribute[@name='Id']" ([ref]$originkey)))
+    $originKey = ""
+    if ((ApplyXPath "//object[@kind='Budget']/attributes/attribute[@name='Id']" ([ref]$originKey)))
     {
-        $attributes.Add("originkey", $originkey)
-        $attributes.Add("cciid", $originkey)
+        $attributes.Add("originkey", $originKey)
+        $attributes.Add("cciid", $originKey)
+        $packageId = $originKey
     }
     else 
     {
         $errors.Add("origin key is null")
     }
     
-    CreateSeoTitle
     CreateSeoName
-    CreatePhotoHead
     CreateShortHed
-    CreateChatter
-    
-    $attributes
+    CreateSimpleAttributes
 }
 
-Function SetStaticProperties ($Asset, $AssetGroupId, $SiteId, $PropertyId, $Type, $TypeId)
+Function CreateFronts()
 {
-    $Asset.AssetGroupId = $AssetGroupId
-    $Asset.SiteId = $SiteId
-    $Asset.PropertyId = $PropertyId
-    $Asset.Type = $Type
-    $Asset.TypeId = $TypeId
-    $Asset.Status = "unspecified"
+    $frontName = ""
+    if (ApplyXPath "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='f-page1-front-1']" ([ref]$frontName))
+    {
+        $front = New-Object BusinessObjects.AssetSpace.Front
+        $front.Location = $frontName
+        $front.HoldUntilDate = $latestPublishedDate
+        $content.Handling.PublishTo.Fronts += $front
+    }
+    
+    if (ApplyXPath "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='f-page1-front-2']" ([ref]$frontName))
+    {
+        $front = New-Object BusinessObjects.AssetSpace.Front
+        $front.Location = $frontName
+        $content.Handling.PublishTo.Fronts += $front
+    }
+    
+    if (ApplyXPath "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='f-page1-front-3']" ([ref]$frontName))
+    {
+        $front = New-Object BusinessObjects.AssetSpace.Front
+        $front.Location = $frontName
+        $content.Handling.PublishTo.Fronts += $front
+    }
+    
+    if (ApplyXPath "//object/children/object[@kind='OnlineArticle']/content/data/CCIObjects/object[@kind='OnlineArticle']/attributes/attribute[@name='f-page1-front-4']" ([ref]$frontName))
+    {
+        $front = New-Object BusinessObjects.AssetSpace.Front
+        $front.Location = $frontName
+        $content.Handling.PublishTo.Fronts += $front
+    }
 }
+
+Function CreateHeadline()
+{
+    $headline = ""
+    $headlineRegEx = New-Object "System.Collections.Generic.List[string[]]"
+    $headlineRegEx.Add(@("{\d+}$", ""))
+    if (!(ApplyXPathAndRegEx "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:mm_head" $headlineRegEx  ([ref]$headline)))
+    {
+        ApplyXPathAndRegEx "//object/children/object[@kind='Text']/content/data/cci:ccitext/cci:head" $headlineRegEx ([ref]$headline)
+    }
+    
+    if ($headline -NE "")
+    {
+        $content.Headline = $headline
+    }
+    else
+    {
+        $content.Headline = "NewsGate package " + $packageId
+    }
+}
+
+Function CreateBody()
+{
+
+}
+
+Function SetStaticProperties ($AssetGroupId, $SiteId, $PropertyId, $Type, $TypeId)
+{
+    $content.AssetGroupId = $AssetGroupId
+    $content.SiteId = $SiteId
+    $content.PropertyId = $PropertyId
+    $content.Type = $Type
+    $content.TypeId = $TypeId
+    $content.Status = "draft"
+}
+
 
 # load external assemblies
  #[Reflection.Assembly]::Load("System.Xml.Linq, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")
@@ -171,7 +211,7 @@ Function SetStaticProperties ($Asset, $AssetGroupId, $SiteId, $PropertyId, $Type
  
 
 # get global parameters
-[xml]$xmlDoc = Get-Content "C:\Users\kkhan\Documents\personal\github\TestApplication\sample_ng.xml"
+[xml]$xmlDoc = Get-Content "C:\Users\kkhan\Documents\personal\github\TestApplication\powershell\sample_ng.xml"
 
 $xml = [System.Xml.Linq.XElement]::Parse($xmlDoc.OuterXml)
 
@@ -185,10 +225,23 @@ $xmlns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
 
 
 # initialize other global variables
+$packageId = ""
+$body = ""
 $attributes = @{}
-$errors = New-Object "System.Collections.Generic.List[string]"
+$errors = New-Object System.Collections.Generic.List[string]
+$latestPublishedDate = ""
+if (!(ApplyXPath "//object[@kind='Budget']/attributes/attribute[@name='PublicationDate']" ([ref]$latestPublishedDate)))
+{
+    $latestPublishedDate = Get-Date
+}
 
-#create attributes
-CreateAttributes $xml
+
+#initialize Content object and set default properties
+SetStaticProperties 1 1 1 "text" 1
+
+#create attributes and other properties
+CreateAttributes
+CreateFronts
+CreateHeadline
 
 #$attributes.GetEnumerator() | Sort-Object Value -descending
